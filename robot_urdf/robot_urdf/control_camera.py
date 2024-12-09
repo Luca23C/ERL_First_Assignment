1a8be83484a3a1ceb59a47d72ed8d71631dc3b05
Luca23C <luca.cornia.lc@gmail.com>
2024-12-09 23:43:23 +0100

    check status for camera control

--- robot_urdf/robot_urdf/control_camera.py
+++ robot_urdf/robot_urdf/control_camera.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64MultiArray
-
+from std_msgs.msg import String
 
 
 class ControlCamera(Node):
@@ -14,22 +14,33 @@ class ControlCamera(Node):
         self.timer = self.create_timer(timer_period, self.timer_callback_camera_control)
         self.angle = 0.0
         self.start = True
+        self.status = True
+
+        # Subscriber for stopping the rover when all the markers are detected
+        self.subscription_status = self.create_subscription(String, '/status', self.listener_status, 10)
 
     def timer_callback_camera_control(self):
         # Routine for controlling camera
         control_msg = Float64MultiArray()
-        if self.start:
-            self.angle = self.angle + 0.1
-            self.start = False
-        else:
-            self.angle = self.angle + 0.1
-        
-        if self.angle > 6.28:
-            self.angle = 0.0
-            self.start = True
+        if self.status:
+            if self.start:
+                self.angle = self.angle + 0.1
+                self.start = False
+            else:
+                self.angle = self.angle + 0.1
+            
+            if self.angle > 6.28:
+                self.angle = 0.0
+                self.start = True
 
-        control_msg.data = [self.angle]
-        self.pub_camera_control.publish(control_msg)
+            control_msg.data = [self.angle]
+            self.pub_camera_control.publish(control_msg)
+
+
+    def listener_status(self, msg):
+        status = msg.data
+        if status == 'Done':
+            self.status = False
 
 
 def main(args=None):
